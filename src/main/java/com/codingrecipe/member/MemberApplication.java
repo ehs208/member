295b81c24864aa03a56858d3d838db886185838e
Eun hyunsu <ehs208@naver.com>
2024-02-16 01:44:24 +0900

    ''

--- src/main/java/com/codingrecipe/member/MemberApplication.java
+++ src/main/java/com/codingrecipe/member/MemberApplication.java
@@ -3,7 +3,7 @@ package com.codingrecipe.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(excludeName = "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration")
 
 public class MemberApplication {
 
